5e5173e8f413dc17e5c781d25d026daf71fcb389
junho <qkrwnsgh1288@daum.net>
2023-05-20 03:45:48 +0900

    add ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: 'nest-server',
       script: 'dist/main.js',
       exec_mode: 'cluster',
-      instances: 0,
+      instances: 1,
       max_memory_restart: '1024M',
     },
   ],
